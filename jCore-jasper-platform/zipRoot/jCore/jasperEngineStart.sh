bcb7c14b00a417496d47f48ca2e27eec93d6ae76
Abraham Wehbi <awehbi@coralcea.ca>
2012-09-25 23:04:30 -0400

    start/stop scripts updated
    updated start and stop scripts

--- jCore-jasper-platform/zipRoot/jCore/jasperEngineStart.sh
+++ jCore-jasper-platform/zipRoot/jCore/jasperEngineStart.sh
@@ -6,4 +6,4 @@ JASPER_ENGINE_LOG4J_xml=config/jCore-engine-log4j.xml
 JASPER_ENGINE_MAIN_CLASS=org.jasper.jCore.engine.JECore
 
 
-$JAVA_HOME/bin/java -ea -DjCore-engine-property-file=$JASPER_ENGINE_PROPERTY_FILE -DjCore-engine-log4j-xml=$JASPER_ENGINE_LOG4J_xml -classpath $JASPER_ENGINE_CLASSPATH $JASPER_ENGINE_MAIN_CLASS
+java -ea -DjCore-engine-property-file=$JASPER_ENGINE_PROPERTY_FILE -DjCore-engine-log4j-xml=$JASPER_ENGINE_LOG4J_xml -classpath $JASPER_ENGINE_CLASSPATH $JASPER_ENGINE_MAIN_CLASS
--- jCore-jasper-platform/zipRoot/jasperStart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd jCore
-./jasperEngineStart.sh &
-cd mule-standalone-3.3.0/bin
-./mule &
-
