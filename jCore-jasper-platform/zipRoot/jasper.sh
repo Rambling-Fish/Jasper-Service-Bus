bc212bf5f7bea90ed8d5396e3f02f2dd7a7786c2
Abraham Wehbi <awehbi@coralcea.ca>
2012-09-26 23:30:23 -0400

    bug fix - script didn't find pid when pid was greater than 4 digits
    bug fix - script didn't find pid when pid was greater than 4 digits, added fix to catch both cases, when pid is longer and when it's shorter than 4 digits
    
    Signed-off-by: Abraham Wehbi <awehbi@coralcea.ca>

--- jCore-jasper-platform/zipRoot/jasper.sh
+++ jCore-jasper-platform/zipRoot/jasper.sh
@@ -4,11 +4,17 @@ J_PID=""
 M_PID=""
 
 function get_j_pid {
-   J_PID=`ps ax | grep java | grep JECore | cut -d " " -f 2`
+    J_PID=`ps ax | grep java | grep JECore | cut -d " " -f 1`
+    if [ -z $J_PID ]; then
+      J_PID=`ps ax | grep java | grep JECore | cut -d " " -f 2`
+    fi
 }
 
 function get_m_pid {
-   M_PID=`ps ax | grep mule | grep wrapper.pidfile | cut -d " " -f 2`
+    M_PID=`ps ax | grep mule | grep wrapper.pidfile | cut -d " " -f 1`
+    if [ -z $M_PID ]; then
+      M_PID=`ps ax | grep mule | grep wrapper.pidfile | cut -d " " -f 2`
+    fi 
 }
 
 function stop_j {
