4216826ab346e5b56d2cc88abe9fcd9957e2e620
nadia Noori <nnoori@coralcea.ca>
2012-10-01 16:04:09 -0400

    Modified jasper.sh and setup.sh script files to setup jasper-management and start the application

--- jCore-jasper-platform/zipRoot/jasper.sh
+++ jCore-jasper-platform/zipRoot/jasper.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
+#initialize PID values
 J_PID=""
 M_PID=""
-
+JM_PID=""
+#Jasper Engine
 function get_j_pid {
     J_PID=`ps ax | grep java | grep JECore | cut -d " " -f 1`
     if [ -z $J_PID ]; then
       J_PID=`ps ax | grep java | grep JECore | cut -d " " -f 2`
     fi
 }
-
+#Mule
 function get_m_pid {
     M_PID=`ps ax | grep mule | grep wrapper.pidfile | cut -d " " -f 1`
     if [ -z $M_PID ]; then
       M_PID=`ps ax | grep mule | grep wrapper.pidfile | cut -d " " -f 2`
     fi 
 }
-
+#Jasper Console
+function get_jm_pid {
+    J_PID=`ps ax | grep java | grep jmanage | cut -d " " -f 1`
+    if [ -z $JM_PID ]; then
+      JM_PID=`ps ax | grep java | grep jmanage | cut -d " " -f 2`
+    fi
+}
+#Jasper Engine 
 function stop_j {
    get_j_pid
    if [ -z $J_PID ]; then
@@ -28,7 +37,7 @@ function stop_j {
       echo ".. Done."
    fi
 }
-
+#Mule
 function stop_m {
    get_m_pid
    if [ -z $M_PID ]; then
@@ -40,7 +49,19 @@ function stop_m {
       echo ".. Done."
    fi
 }
-
+#Jasper Console
+function stop_jm {
+   get_jm_pid
+   if [ -z $M_PID ]; then
+      echo "Jasper Management Console is not running." 
+   else
+      echo -n "Stopping Jasper Management Console.."
+      kill $M_PID 
+      sleep 1
+      echo ".. Done."
+   fi
+}
+#Jasper Engine
 function start_j {
    get_j_pid
 if [ -z $J_PID ]; then
@@ -54,7 +75,7 @@ if [ -z $J_PID ]; then
       echo "Jasper Engine is already running, PID=$J_PID"
    fi
 }
-
+#Mule
 function start_m {
    get_m_pid
 if [ -z $M_PID ]; then
@@ -66,6 +87,20 @@ if [ -z $M_PID ]; then
       echo "Done. PID=$M_PID"
    else
       echo "jApps Server is already running, PID=$M_PID"
+   fi
+}
+#Jasper Management Console
+function start_jm {
+   get_jm_pid
+if [ -z $JM_PID ]; then
+      echo  "Starting Jasper ManagementConsole.."
+      cd jasper-management/bin
+      ./startup.sh 123456 &
+      cd ../../../
+      get_jm_pid
+      echo "Done. PID=$JM_PID"
+   else
+      echo "Jasper Management Console is already running, PID=$JM_PID"
    fi
 }
 
@@ -86,11 +121,22 @@ function status_m {
       echo "jApps Server is running, PID=$M_PID"
    fi
 }
+#Jasper Management Console 
+function status_jm {
+   get_jm_pid
+   if [ -z  $M_PID ]; then
+      echo "Jasper Management Console is not running." 
+   else
+      echo "asper Management Console is running, PID=$JM_PID"
+   fi
+}
 
 function start {
     start_j
     sleep 5
     start_m
+    sleep 5
+    start_jm
     exit 1
 }
 
@@ -98,12 +144,15 @@ function stop {
     stop_m
     sleep 5
     stop_j
+    sleep 5
+    stop_jm
     exit 1
 }
 
 function status {
     status_j
     status_m
+    status_jm
     exit 1
 }
 
--- jCore-jasper-platform/zipRoot/setup.sh
+++ jCore-jasper-platform/zipRoot/setup.sh
@@ -5,3 +5,6 @@ cd jCore
 tar xvfz mule-standalone-3.3.0.tar.gz
 rm mule-standalone-3.3.0.tar.gz
 chmod 755 jasperEngineStart.sh
+cd ..
+unzip jasper-management.zip
+rm jasper-management.zip
