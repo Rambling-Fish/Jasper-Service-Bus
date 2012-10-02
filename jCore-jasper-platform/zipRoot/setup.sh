adea44866cda8892a5b9ce13b802a40676aa577d
nadia Noori <nnoori@coralcea.ca>
2012-10-02 16:12:53 -0400

    Change the POM files for the Children projects to put jApps in /jApps folder and modify the Parent POM to include the jasper-management archive as well as the jApp-management to enable the JMX - change the setup.sh script file as well

--- jCore-jasper-platform/zipRoot/setup.sh
+++ jCore-jasper-platform/zipRoot/setup.sh
@@ -6,5 +6,9 @@ tar xvfz mule-standalone-3.3.0.tar.gz
 rm mule-standalone-3.3.0.tar.gz
 chmod 755 jasperEngineStart.sh
 cd ..
-unzip jasper-management.zip
-rm jasper-management.zip
+cp -R ../jApps/jApp-management jCore/mule-standalone-3.3.0/apps/
+rm -R ../jApps/jApp-management
+cp ../jApps/*.* jApps/
+cp ../jasper-management.tar.gz ./
+tar -pxvzf jasper-management.tar.gz
+rm jasper-management.tar.gz
