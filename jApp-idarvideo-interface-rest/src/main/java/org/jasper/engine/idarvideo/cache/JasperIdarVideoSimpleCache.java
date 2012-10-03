43615ab602699d785b698ae6a283f52e40c5338d
Ray Ouellette <rouellette@coralcea.ca>
2012-10-03 09:30:32 -0400

    error handling
    check for null pointer if ivm message is null. Log if exception is caught

--- jApp-idarvideo-interface-rest/src/main/java/org/jasper/engine/idarvideo/cache/JasperIdarVideoSimpleCache.java
+++ jApp-idarvideo-interface-rest/src/main/java/org/jasper/engine/idarvideo/cache/JasperIdarVideoSimpleCache.java
@@ -1,12 +1,20 @@
 package org.jasper.engine.idarvideo.cache;
 
 import org.jasper.jLib.idar.video.codec.IDARVideoMessage;
+import org.apache.log4j.Logger;
 
 public class JasperIdarVideoSimpleCache {
 	private static IDARVideoMessage ivm;
+	private static Logger logger = Logger.getLogger("org.jasper");
 	
-	public Object getRestReponse(String msg){
-		if(msg.equals("/jasper/idarvideo/1.0/trackInfo")) return ivm.getIntelliDARinfo();;
+	public Object getRestResponse(String msg){
+		if(msg.equals("/jasper/idarvideo/1.0/trackInfo")) {
+			try {
+				return ivm.getIntelliDARinfo();
+			} catch (NullPointerException npe) {
+				logger.error("Idar Video Message is null");
+			}
+		}
 		//if(msg.equals("/jasper/idarvideo/1.0/video")) return ivm.getIDARjpeg();
 		return "JASPER ASB --- Resource Not Found.";
 	}
