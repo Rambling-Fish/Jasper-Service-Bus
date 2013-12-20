345526bcdcca1efbe7491b4ad5495e4a2d01d698
Jorge Morales <jorge@magneticore.com>
2013-12-20 15:14:44 -0500

    JASPER-528 in Design Branch
    All the keys (UDE/DTAs) are now stored in the same folder: jsb-core/keystore. Update your scripts!
    
    Signed-off-by: Jorge Morales <jorge@magneticore.com>

--- jasper-platform/zipRoot/setup.sh
+++ jasper-platform/zipRoot/setup.sh
@@ -12,11 +12,11 @@ chmod 600 config/jsb.jmxremote.password
 mv *.jar libs/
 mkdir activemq-data
 if [ ! -L exec/activemq-data ]
-  then  
+  then
     cd exec
     ln -s ../activemq-data .
     cd ..
-fi    
+fi
 }
 
 function setup_jmc_discovery {
@@ -27,13 +27,13 @@ function setup_jmc_discovery {
     tar -xzf jmp.tar.gz
     rm jmp.tar.gz
     cd $CDIR
-  fi  
+  fi
 }
 
 function setup_dta {
 if [ ! -e ../jmp ]; then
   setup_jmc_discovery
-fi  
+fi
 mkdir -p DTAs
 mkdir -p logs
 cd jsb-core
@@ -45,7 +45,7 @@ if ! [ -d mule-standalone-3.4.0 ]; then
    mv default mule-standalone-3.4.0/apps/
    rm default.tar.gz
    cd mule-standalone-3.4.0
-   ln -s ../keystore/DTAs keystore
+   ln -s ../keystore keystore
 fi
 }
 
@@ -63,7 +63,7 @@ if ! [ -d mule-standalone-3.4.0 ]; then
    mv default mule-standalone-3.4.0/apps/
    rm default.tar.gz
    cd mule-standalone-3.4.0
-   ln -s ../keystore/DTAs keystore
+   ln -s ../keystore keystore
    cd ..
    chmod 600 config/jsb.jmxremote.password
    mv *.jar libs/
@@ -74,7 +74,7 @@ chmod 755 udeAutoStart
 chmod 755 dtaAutoStart
 mkdir activemq-data
 if [ ! -L exec/activemq-data ]
-  then  
+  then
     cd exec
     ln -s ../activemq-data .
     cd ..
@@ -92,5 +92,5 @@ case "$1" in
         setup_all
         ;;
     *) echo "Usage: $0 {ude|dta|all}"
-;;    
+;;
 esac
