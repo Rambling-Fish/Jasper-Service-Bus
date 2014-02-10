0c8c6a5394fc1abecb12f8f927d06b5b06baf462
Ray Ouellette <rouellette@coralcea.ca>
2014-02-10 15:37:49 -0500

    JASPER-540
    Beef up test coverage of DataHandler class

--- jasper-platform/jsb/src/test/java/org/jasper/core/delegate/handlers/TestDataHandler.java
+++ jasper-platform/jsb/src/test/java/org/jasper/core/delegate/handlers/TestDataHandler.java
@@ -38,16 +38,21 @@ public class TestDataHandler extends TestCase {
 	private String contentType = "application/json";
 	private String version = "1.0";
 	private String deploymentAndInstance = "UDE:0";
-	private String hrDataReqeuest = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\"},\"parameters\":{\"http://coralcea.ca/jasper/hrSID\":\"1\"}}";
-    private String hrDataNotification = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\",\"expires\":\"10\"},\"parameters\":{},\"rule\":\"compareint(http://coralcea.ca/jasper/medicalSensor/heartRate/data/bpm,gt,90)\"}";
-	private String noRURIReqeuest = "{\"version\":\"1.0\",\"method\":\"GET\",\"headers\":{\"content-type\":\"application/json\"},\"parameters\":{\"http://coralcea.ca/jasper/hrSID\":\"1\"}}";
+	private String hrDataReqeuest = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\"},\"parameters\":{\"http://coralcea.ca/jasper/hrSID\":\"1\",\"http://coralcea.ca/jasper/msID\":\"001\"}}";
+    private String hrDataNotification = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\",\"expires\":\"10\",\"output\":\"json\"},\"parameters\":{},\"rule\":\"compareint(http://coralcea.ca/jasper/medicalSensor/heartRate/data/bpm,gt,90)\"}";
+    private String hrDataNotificationNoExpires = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\"},\"parameters\":{},\"rule\":\"compareint(http://coralcea.ca/jasper/medicalSensor/heartRate/data/bpm,gt,90)\"}";
+    private String hrDataNotificationMinPolling = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\",\"expires\":\"10\",\"poll-period\":\"1\"},\"parameters\":{},\"rule\":\"compareint(http://coralcea.ca/jasper/medicalSensor/heartRate/data/bpm,gt,90)\"}";
+    private String hrDataNotificationMaxPolling = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\",\"expires\":\"5\",\"poll-period\":\"15\"},\"parameters\":{},\"rule\":\"compareint(http://coralcea.ca/jasper/medicalSensor/heartRate/data/bpm,gt,90)\"}";
+    private String hrDataNotificationBadInts = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\",\"expires\":\"1x\",\"output\":\"json\",\"poll-period\":\"Y\"},\"parameters\":{},\"rule\":\"compareint(http://coralcea.ca/jasper/medicalSensor/heartRate/data/bpm,gt,90)\"}";
+    private String hrDataNotificationInvalidTrigger = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\",\"expires\":\"10\",\"output\":\"json\"},\"parameters\":{},\"rule\":\"comparelong(http://coralcea.ca/jasper/medicalSensor/heartRate/data/bpm,gt,90)\"}";
+    private String noRURIReqeuest = "{\"version\":\"1.0\",\"method\":\"GET\",\"headers\":{\"content-type\":\"application/json\"},\"parameters\":{\"http://coralcea.ca/jasper/hrSID\":\"1\"}}";
 	private String nullRURIReqeuest = "{\"version\":\"1.0\",\"method\":\"GET\",\"ruri\":\"\",\"headers\":{\"content-type\":\"application/json\"},\"parameters\":{\"http://coralcea.ca/jasper/hrSID\":\"1\"}}";
 	private String unsupportedMethodReqeuest = "{\"version\":\"1.0\",\"method\":\"DELETE\",\"ruri\":\"http://coralcea.ca/jasper/hrData\",\"headers\":{\"content-type\":\"application/json\"},\"parameters\":{\"http://coralcea.ca/jasper/hrSID\":\"1\"}}";
 
 	
 	/*
 	 * This tests the Data Handler error handling when receiving
-	 * a null request
+	 * a null request (i.e. no text in text message)
 	 */
 	@Test
 	public void testNullRequest() throws Exception{
@@ -78,6 +83,87 @@ public class TestDataHandler extends TestCase {
 	public void testValidNotification() throws Exception{
 		when(mockRequest.getText()).thenReturn(hrDataNotification);
 		when(mockRequest.getJMSReplyTo()).thenReturn(null);
+		mockDelegate.defaultOutput = "json";
+
+		classUnderTest.run();	
+	}
+	
+	/*
+	 * This tests the Data Handler receiving a valid notification
+	 * but with no expires parameter
+	 */
+	@Test
+	public void testNotificationNoExpires() throws Exception{
+		when(mockRequest.getText()).thenReturn(hrDataNotificationNoExpires);
+		when(mockRequest.getJMSReplyTo()).thenReturn(null);
+		mockDelegate.defaultOutput = "json";
+		mockDelegate.maxExpiry = 60000;
+		mockDelegate.maxPollingInterval = 6000;
+		mockDelegate.minPollingInterval = 2000;
+
+		classUnderTest.run();	
+	}
+	
+	/*
+	 * This tests the Data Handler receiving a valid notification
+	 * but with a poll period that is less than the minimum
+	 */
+	@Test
+	public void testNotificationMinPolling() throws Exception{
+		when(mockRequest.getText()).thenReturn(hrDataNotificationMinPolling);
+		when(mockRequest.getJMSReplyTo()).thenReturn(null);
+		mockDelegate.defaultOutput = "json";
+		mockDelegate.maxExpiry = 60000;
+		mockDelegate.maxPollingInterval = 6000;
+		mockDelegate.minPollingInterval = 2000;
+
+		classUnderTest.run();	
+	}
+	
+	/*
+	 * This tests the Data Handler receiving a valid notification
+	 * but with a poll period that is greater than maximum
+	 */
+	@Test
+	public void testNotificationMaxPolling() throws Exception{
+		when(mockRequest.getText()).thenReturn(hrDataNotificationMaxPolling);
+		when(mockRequest.getJMSReplyTo()).thenReturn(null);
+		mockDelegate.defaultOutput = "json";
+		mockDelegate.maxExpiry = 60000;
+		mockDelegate.maxPollingInterval = 6000;
+		mockDelegate.minPollingInterval = 2000;
+
+		classUnderTest.run();	
+	}
+	
+	/*
+	 * This tests the Data Handler receiving a valid notification
+	 * but with non numbers in the poll period and expires parameters
+	 */
+	@Test
+	public void testNotificationBadInts() throws Exception{
+		when(mockRequest.getText()).thenReturn(hrDataNotificationBadInts);
+		when(mockRequest.getJMSReplyTo()).thenReturn(null);
+		mockDelegate.defaultOutput = "json";
+		mockDelegate.maxExpiry = 60000;
+		mockDelegate.maxPollingInterval = 6000;
+		mockDelegate.minPollingInterval = 2000;
+
+		classUnderTest.run();	
+	}
+	
+	/*
+	 * This tests the Data Handler receiving an invalid notification
+	 * with an unknown trigger name
+	 */
+	@Test
+	public void testNotificationInvalidTrigger() throws Exception{
+		when(mockRequest.getText()).thenReturn(hrDataNotificationInvalidTrigger);
+		when(mockRequest.getJMSReplyTo()).thenReturn(null);
+		mockDelegate.defaultOutput = "json";
+		mockDelegate.maxExpiry = 60000;
+		mockDelegate.maxPollingInterval = 6000;
+		mockDelegate.minPollingInterval = 2000;
 
 		classUnderTest.run();	
 	}
