4aaf5deff580dc44efea560941a66e2d2e18f9d3
Ray Ouellette <rouellette@coralcea.ca>
2014-04-24 11:48:03 -0400

    Json-LD transformer Test class
    JUnit class plus minor changes uncovered by testing

--- jasper-platform/jsb/src/main/java/org/jasper/core/util/JsonLDTransformer.java
+++ jasper-platform/jsb/src/main/java/org/jasper/core/util/JsonLDTransformer.java
@@ -1,22 +1,18 @@
 package org.jasper.core.util;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class JsonLDTransformer {
-	private static JsonObject output= new JsonObject();
-	private static JsonObject globalContext = new JsonObject();
+	private JsonObject output;
+	private JsonObject globalContext;
 
 	public JsonLDTransformer() {}
 	
-	private static String parseUri(String uri, JsonObject context){
+	private String parseUri(String uri, JsonObject context){
 		String[] tmp = uri.split("/");
 		String element = tmp[tmp.length-1];
 		if(context.has(element)){
@@ -27,8 +23,11 @@ public class JsonLDTransformer {
 	}
 	
 	public JsonElement parseResponse(JsonElement response){
+		output = new JsonObject();
+		globalContext = new JsonObject();
 		JsonObject jsonObj = new JsonObject();
 		String arrayName = null;
+		
 		if(response.isJsonArray()){
 			if(response.getAsJsonArray().get(0).isJsonPrimitive()){
 				return response;
@@ -37,7 +36,10 @@ public class JsonLDTransformer {
 				return parseArray(null, response.getAsJsonArray(), false);
 			}
 		}
-		if(response.isJsonObject()){
+		else if(response.isJsonPrimitive()){
+			return response;
+		}
+		else if(response.isJsonObject()){
 			jsonObj = response.getAsJsonObject();
 			output.add("@context",  globalContext);
 		}
@@ -55,6 +57,9 @@ public class JsonLDTransformer {
 				globalContext.addProperty(parsedUri,entry.getKey());
 				output.add(parsedUri, entry.getValue());
 			}
+			else if(!entry.getValue().isJsonArray()){
+				output.add(entry.getKey(), entry.getValue());
+			}
 			else{
 				if(entry.getValue().getAsJsonArray().get(0).isJsonPrimitive()){
 					globalContext.addProperty(parsedUri,entry.getKey());
@@ -71,7 +76,7 @@ public class JsonLDTransformer {
 		return output;	
 	}
 	
-	private static JsonElement parseArray(String arrayName, JsonArray jsonArr, boolean useGlobalCtx){
+	private JsonElement parseArray(String arrayName, JsonArray jsonArr, boolean useGlobalCtx){
 		JsonArray newArray = new JsonArray();
 		String parsedUri = null;
 		for(JsonElement item:jsonArr){
@@ -90,6 +95,7 @@ public class JsonLDTransformer {
 				}
 				if((useGlobalCtx) && (parsedUri != null)){
 					globalContext.addProperty(parsedUri,arrayEntry.getKey());
+					tmpObj.add(parsedUri, arrayEntry.getValue());
 				}
 				else if(parsedUri != null){
 					localContext.addProperty(parsedUri,arrayEntry.getKey());
@@ -110,59 +116,6 @@ public class JsonLDTransformer {
 		else{
 			return newArray;
 		}
-	}
-
-
-	public static void main(String[] args) {
-		JsonLDTransformer ldTransformer = new JsonLDTransformer();
-		JsonParser jParser = new JsonParser();
-		JsonObject jsonObj = new JsonObject();
-		JsonArray jsonArr  = new JsonArray();
-		JsonArray mixedArr = new JsonArray();
-		JsonArray primitiveArr  = new JsonArray();
-		JsonElement prim1 = jParser.parse("one");
-		JsonElement prim2 = jParser.parse("two");
-		primitiveArr.add(prim1);
-		primitiveArr.add(prim2);
-		Map<String,String> map = new HashMap<String,String>();
-		Map<String,String> map2 = new HashMap<String,String>();
-		Gson gson = new Gson();
-		map.put("http://coralcea.ca/demo/bpm", "52");
-		map.put("http://coralcea.ca/demo/timestamp", "2014-04-16");
-		map.put("http://coralcea.ca/demo/patient/id", "http://pid01");
-		map.put("http://coralcea.ca/demo/sensor/id", "srID02");
-		
-		JsonElement jsonTree = gson.toJsonTree(map, Map.class);
-		jsonArr.add(jsonTree);
-		mixedArr.add(jsonTree);
-		map.put("http://coralcea.ca/demo/bpm", "99");
-		map.put("http://coralcea.ca/demo/timestamp", "2014-04-17");
-		map.put("http://coralcea.ca/demo/patient/id", "http://pid99");
-		map.put("http://coralcea.ca/demo/sensor/id", "srID03");
-		jsonTree = gson.toJsonTree(map, Map.class);
-		jsonArr.add(jsonTree);
-		
-		map2.put("http://coralcea.ca/demo/diastolic", "120");
-		map2.put("http://coralcea.ca/demo/timestamp", "2014-05-12");
-		map2.put("http://coralcea.ca/demo/systolic", "85");
-		jsonTree = gson.toJsonTree(map2, Map.class);
-		mixedArr.add(jsonTree);
-
-		
-		jsonObj.addProperty("http://coralcea.ca/demo/wardId", "Wing-5-Floor-3-Ward-4");
-		jsonObj.addProperty("http://coralcea.ca/demo/data", "some-data");
-		jsonObj.addProperty("http://coralcea.ca/demo/more/data", "more-data");
-		jsonObj.add("http://coralcea.ca/demo/patient/data", jsonArr);
-		jsonObj.add("http://coralcea.ca/demo/patient/primitive/data", primitiveArr);
-		
-		System.out.println("Input - simple primitive array:\n" + primitiveArr.toString());
-		System.out.println("Parsed response:\n" + ldTransformer.parseResponse(primitiveArr));
-		System.out.println("\nInput - object with embedded non-primitive and primitive arrays:\n" + jsonObj);
-		System.out.println("Parsed response:\n" + ldTransformer.parseResponse(jsonObj));
-		System.out.println("\nInput - non-primitive array:\n" + jsonArr.toString());
-		System.out.println("Parsed response:\n" + ldTransformer.parseResponse(jsonArr));
-		System.out.println("\nInput - mixed non-primitive array:\n" + mixedArr.toString());
-		System.out.println("Parsed response :\n" + ldTransformer.parseResponse(mixedArr));
 	}
 
 }
