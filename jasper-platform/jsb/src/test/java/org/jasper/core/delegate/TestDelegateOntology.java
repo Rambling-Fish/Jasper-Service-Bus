449ff1aa87bdacc7a48b9266b67fe292087e4440
Paul Granville <pgranville@coralcea.ca>
2014-05-16 17:18:07 -0400

    JASPER-623 #time 30m #comment Fix triples and queryStrings to reflect vocab changes (dta:input -> dta:parameter and dta:output to dta:data).  Also implement TODO comment (ie, assertNotNull at line 133)
    Signed-off-by: Paul Granville <pgranville@coralcea.ca>

--- jasper-platform/jsb/src/test/java/org/jasper/core/delegate/TestDelegateOntology.java
+++ jasper-platform/jsb/src/test/java/org/jasper/core/delegate/TestDelegateOntology.java
@@ -37,8 +37,8 @@ public class TestDelegateOntology {
 	private static String dtaName = "jasper:dta-heart-rate-monitor-D:1.0:jasperLab";
 	private static String dtaName2 = "jasper:dta-sms-terminator:1.0:rayLab";
 	private String dest = "dta-heart-rate-monitor-d-gethrdata";
-	private String triples = "@prefix :  <http://coralcea.ca/jasper/> .\n@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n@prefix hrSensor: <http://coralcea.ca/jasper/hrSensor/> .\n@prefix xsd:   <http://www.w3.org/2001/XMLSchema#> .\n@prefix owl:   <http://www.w3.org/2002/07/owl#> .\n@prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n@prefix dta:   <http://coralcea.ca/2014/01/dta#> .\n\n:MsData  a  owl:Class .\n\n:timestamp  a  owl:DatatypeProperty ;\n  rdfs:domain  :HrData ;\n rdfs:range   xsd:string .\n\n:hrSID  a owl:DatatypeProperty ;\n rdfs:domain  :HrDataReq ;\n  rdfs:range   xsd:string .\n\n:HrData  a owl:Class ;\n rdfs:subClassOf  :MsData ;\n  dta:restriction  [ a  owl:Restriction ;\n  owl:onProperty :timestamp\n  ] ;\n dta:restriction  [ a  owl:Restriction ;\n owl:onProperty   hrSensor:bpm\n ] .\n\n:dta-heart-rate-monitor-D\n a dta:DTA ;\n dta:operation  :getHrData .\n\n:hrData  a owl:ObjectProperty ;\n  rdfs:range :HrData ;\n  rdfs:subPropertyOf  :msData .\n\n:msData  a owl:ObjectProperty ;\n rdfs:range  :MsData .\n\n:HrDataReq  a owl:Class ;\n  dta:restriction  [ a owl:Restriction ;\n owl:onProperty   :hrSID;\n ] .\n\nhrSensor:bpm  a owl:DatatypeProperty ;\n  rdfs:domain  :HrData ;\n rdfs:range   xsd:integer .\n\n:getHrData  a dta:Operation ;\n dta:destination  'dta-heart-rate-monitor-d-gethrdata' ;\n dta:input :HrDataReq ;\n dta:kind dta:Get ;\n  dta:output :hrData .\n";	
-	private String triples2 = "@prefix : <http://coralcea.ca/jasper/Sms/> .\n@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n@prefix xsd:   <http://www.w3.org/2001/XMLSchema#> .\n@prefix owl:   <http://www.w3.org/2002/07/owl#> .\n@prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n@prefix jasper: <http://coralcea.ca/jasper/> .\n@prefix dta:   <http://coralcea.ca/2014/01/dta#> .\n\n:SmsPostReq  a     owl:Class ;\n  dta:restriction  [ a  owl:Restriction ;\n   owl:onProperty   :bodySms\n  ] ;\n  dta:restriction  [ a  owl:Restriction ;\n    owl:onProperty   :fromSms\n   ] ;\n  dta:restriction  [ a  owl:Restriction ;\n  owl:onProperty  :logId\n ] ;\n  dta:restriction  [ a  owl:Restriction ;\n  owl:onProperty   :toSms\n  ] .\n\n:dta-sms-terminator  a  dta:DTA ;\n dta:operation  :sendSms .\n\n:sendSms  a  dta:Operation ;\n  dta:destination  'dta-sms-terminator-sendsms' ;\n  dta:input  :SmsPostReq ;\n  dta:kind  dta:Post .\n\n:fromSms  a owl:DatatypeProperty ;\n  rdfs:domain  :SmsPostReq ;\n  rdfs:range   xsd:string .\n\n:logId  a  owl:DatatypeProperty ;\n rdfs:domain  :SmsPostReq ;\n  rdfs:range   xsd:string .\n\n:toSms  a owl:DatatypeProperty ;\n  rdfs:domain  :SmsPostReq ;\n rdfs:range   xsd:string .\n\n:bodySms  a owl:DatatypeProperty ;\n rdfs:domain  :SmsPostReq ;\n rdfs:range   xsd:string .\n";	
+	private String triples = "@prefix :  <http://coralcea.ca/jasper/> .\n@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n@prefix hrSensor: <http://coralcea.ca/jasper/hrSensor/> .\n@prefix xsd:   <http://www.w3.org/2001/XMLSchema#> .\n@prefix owl:   <http://www.w3.org/2002/07/owl#> .\n@prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n@prefix dta:   <http://coralcea.ca/2014/01/dta#> .\n\n:MsData  a  owl:Class .\n\n:timestamp  a  owl:DatatypeProperty ;\n  rdfs:domain  :HrData ;\n rdfs:range   xsd:string .\n\n:hrSID  a owl:DatatypeProperty ;\n rdfs:domain  :HrDataReq ;\n  rdfs:range   xsd:string .\n\n:HrData  a owl:Class ;\n rdfs:subClassOf  :MsData ;\n  dta:restriction  [ a  owl:Restriction ;\n  owl:onProperty :timestamp\n  ] ;\n dta:restriction  [ a  owl:Restriction ;\n owl:onProperty   hrSensor:bpm\n ] .\n\n:dta-heart-rate-monitor-D\n a dta:DTA ;\n dta:operation  :getHrData .\n\n:hrData  a owl:ObjectProperty ;\n  rdfs:range :HrData ;\n  rdfs:subPropertyOf  :msData .\n\n:msData  a owl:ObjectProperty ;\n rdfs:range  :MsData .\n\n:HrDataReq  a owl:Class ;\n  dta:restriction  [ a owl:Restriction ;\n owl:onProperty   :hrSID;\n ] .\n\nhrSensor:bpm  a owl:DatatypeProperty ;\n  rdfs:domain  :HrData ;\n rdfs:range   xsd:integer .\n\n:getHrData  a dta:Operation ;\n dta:destination  'dta-heart-rate-monitor-d-gethrdata' ;\n dta:parameter :HrDataReq ;\n dta:kind dta:Get ;\n  dta:data :hrData .\n";	
+	private String triples2 = "@prefix : <http://coralcea.ca/jasper/Sms/> .\n@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n@prefix xsd:   <http://www.w3.org/2001/XMLSchema#> .\n@prefix owl:   <http://www.w3.org/2002/07/owl#> .\n@prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n@prefix jasper: <http://coralcea.ca/jasper/> .\n@prefix dta:   <http://coralcea.ca/2014/01/dta#> .\n\n:SmsPostReq  a     owl:Class ;\n  dta:restriction  [ a  owl:Restriction ;\n   owl:onProperty   :bodySms\n  ] ;\n  dta:restriction  [ a  owl:Restriction ;\n    owl:onProperty   :fromSms\n   ] ;\n  dta:restriction  [ a  owl:Restriction ;\n  owl:onProperty  :logId\n ] ;\n  dta:restriction  [ a  owl:Restriction ;\n  owl:onProperty   :toSms\n  ] .\n\n:dta-sms-terminator  a  dta:DTA ;\n dta:operation  :sendSms .\n\n:sendSms  a  dta:Operation ;\n  dta:destination  'dta-sms-terminator-sendsms' ;\n  dta:parameter  :SmsPostReq ;\n  dta:kind  dta:Post .\n\n:fromSms  a owl:DatatypeProperty ;\n  rdfs:domain  :SmsPostReq ;\n  rdfs:range   xsd:string .\n\n:logId  a  owl:DatatypeProperty ;\n rdfs:domain  :SmsPostReq ;\n  rdfs:range   xsd:string .\n\n:toSms  a owl:DatatypeProperty ;\n  rdfs:domain  :SmsPostReq ;\n rdfs:range   xsd:string .\n\n:bodySms  a owl:DatatypeProperty ;\n rdfs:domain  :SmsPostReq ;\n rdfs:range   xsd:string .\n";	
 
 	
 	/**
@@ -89,65 +89,64 @@ public class TestDelegateOntology {
 		TestCase.assertEquals(false, result);
 	}
 	
-//	/**
-//	 * This method tests all the public GET methods of the DelegateOntology class
-//	 * Decided to test all methods in a single test case to save time having to
-//	 * start/stop hazelcast between each test.
-//	 * 
-//	 * @throws Exception
-//	 */
-//	@Test
-//	public void testAllGetOperations() throws Exception {
-//		ArrayList<String> results = classUnderTest.getProvideOperations(null);
-//		TestCase.assertEquals(null, results);
-//		
-//		loadModel();
-//		Thread.sleep(50);
-//		results = classUnderTest.getProvideOperations(ruri);
-//		TestCase.assertEquals(1, results.size());
-//		
-//		String result = classUnderTest.getProvideDestinationQueue(null);
-//		TestCase.assertEquals(null, result);
-//		result = classUnderTest.getProvideDestinationQueue(operation);
-//		TestCase.assertEquals(dest, result);
-//		
-//		result = classUnderTest.getProvideOperationInputObject(null);
-//		TestCase.assertEquals(null, result);
-//		result = classUnderTest.getProvideOperationInputObject(operation);
-//		TestCase.assertEquals(inputObject, result);
-//		
-//		String[] strArr = classUnderTest.getSerializedModels();
-//		TestCase.assertNotNull(strArr);
-//		
-//		Set<String>resultSet = classUnderTest.getSuperProperties(ruri);
-//		TestCase.assertEquals(1, resultSet.size());
-//		Iterator<String> it = resultSet.iterator();
-//		String superRuri = null;
-//		while(it.hasNext()){
-//			superRuri = it.next();
-//		}
-//		resultSet.clear();
-//		resultSet = classUnderTest.getEquivalentProperties(superRuri);
-//		//TODO fix this when Maged/Abe fix code. This should not be null
-//		TestCase.assertNull(resultSet);
-//		
-//		ArrayList<String> arrList = classUnderTest.fetchPostOperations(publishUri);
-//		TestCase.assertNotNull(arrList);
-//		arrList = classUnderTest.fetchPostOperations(null);
-//		TestCase.assertNull(arrList);
-//		
-//		String oper = "http://coralcea.ca/jasper/Sms/sendSms";
-//		String inputObj = classUnderTest.fetchPostOperationInputObject(oper);
-//		TestCase.assertEquals("http://coralcea.ca/jasper/Sms/SmsPostReq", inputObj);
-//		inputObj = classUnderTest.fetchPostOperationInputObject(null);
-//		TestCase.assertNull(inputObj);
-//		
-//		String destQ = classUnderTest.fetchPostDestinationQueue(oper);
-//		TestCase.assertEquals("dta-sms-terminator-sendsms", destQ);
-//		destQ = classUnderTest.fetchPostDestinationQueue(null);
-//		TestCase.assertNull(destQ);
-//			
-//	}
+	/**
+	 * This method tests all the public GET methods of the DelegateOntology class
+	 * Decided to test all methods in a single test case to save time having to
+	 * start/stop hazelcast between each test.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testAllGetOperations() throws Exception {
+		ArrayList<String> results = classUnderTest.getProvideOperations(null);
+		TestCase.assertEquals(null, results);
+		
+		loadModel();
+		Thread.sleep(50);
+		results = classUnderTest.getProvideOperations(ruri);
+		TestCase.assertEquals(1, results.size());
+		
+		String result = classUnderTest.getProvideDestinationQueue(null);
+		TestCase.assertEquals(null, result);
+		result = classUnderTest.getProvideDestinationQueue(operation);
+		TestCase.assertEquals(dest, result);
+		
+		result = classUnderTest.getProvideOperationInputObject(null);
+		TestCase.assertEquals(null, result);
+		result = classUnderTest.getProvideOperationInputObject(operation);
+		TestCase.assertEquals(inputObject, result);
+		
+		String[] strArr = classUnderTest.getSerializedModels();
+		TestCase.assertNotNull(strArr);
+		
+		Set<String>resultSet = classUnderTest.getSuperProperties(ruri);
+		TestCase.assertEquals(1, resultSet.size());
+		Iterator<String> it = resultSet.iterator();
+		String superRuri = null;
+		while(it.hasNext()){
+			superRuri = it.next();
+		}
+		resultSet.clear();
+		resultSet = classUnderTest.getEquivalentProperties(superRuri);
+		TestCase.assertNotNull(resultSet);
+		
+		ArrayList<String> arrList = classUnderTest.fetchPostOperations(publishUri);
+		TestCase.assertNotNull(arrList);
+		arrList = classUnderTest.fetchPostOperations(null);
+		TestCase.assertNull(arrList);
+		
+		String oper = "http://coralcea.ca/jasper/Sms/sendSms";
+		String inputObj = classUnderTest.fetchPostOperationInputObject(oper);
+		TestCase.assertEquals("http://coralcea.ca/jasper/Sms/SmsPostReq", inputObj);
+		inputObj = classUnderTest.fetchPostOperationInputObject(null);
+		TestCase.assertNull(inputObj);
+		
+		String destQ = classUnderTest.fetchPostDestinationQueue(oper);
+		TestCase.assertEquals("dta-sms-terminator-sendsms", destQ);
+		destQ = classUnderTest.fetchPostDestinationQueue(null);
+		TestCase.assertNull(destQ);
+			
+	}
 	
 	@Test
 	public void testCreateJsonSchema() throws Exception {
@@ -165,7 +164,7 @@ public class TestDelegateOntology {
 		
 		loadModel();
 		Thread.sleep(50);
-		String queryString = "PREFIX : <http://coralcea.ca/jasper/vocabulary/>\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nPREFIX jta: <http://coralcea.ca/jasper/vocabulary/jta>\nPREFIX param: <http://coralcea.ca/jasper/vocabulary/param>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nPREFIX provides: <http://coralcea.ca/jasper/vocabulary/provides>\nPREFIX jasper: <http://coralcea.ca/jasper/>\nPREFIX dta: <http://coralcea.ca/2014/01/dta#>\nPREFIX requires: <http://coralcea.ca/jasper/vocabulary/requires>\nSELECT ?operation  WHERE \n   {{\n      ?dta              a                dta:DTA        .\n      ?dta              dta:operation    ?operation     .\n      ?operation        dta:kind         dta:Get        .\n      ?operation        dta:output/rdfs:subPropertyOf*       <http://coralcea.ca/jasper/hrData>  .\n       }       UNION       {      ?dta           a                dta:DTA        .\n      ?dta           dta:operation    ?operation     .\n      ?operation     dta:kind         dta:Get        .\n      ?operation     dta:output/rdfs:subPropertyOf*       ?superRuri     .\n      ?superRuri     rdfs:range       ?superType     .\n      <http://coralcea.ca/jasper/hrData> rdfs:domain      ?superType     .\n   }}";
+		String queryString = "PREFIX : <http://coralcea.ca/jasper/vocabulary/>\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nPREFIX jta: <http://coralcea.ca/jasper/vocabulary/jta>\nPREFIX param: <http://coralcea.ca/jasper/vocabulary/param>\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nPREFIX provides: <http://coralcea.ca/jasper/vocabulary/provides>\nPREFIX jasper: <http://coralcea.ca/jasper/>\nPREFIX dta: <http://coralcea.ca/2014/01/dta#>\nPREFIX requires: <http://coralcea.ca/jasper/vocabulary/requires>\nSELECT ?operation  WHERE \n   {{\n      ?dta              a                dta:DTA        .\n      ?dta              dta:operation    ?operation     .\n      ?operation        dta:kind         dta:Get        .\n      ?operation        dta:data/rdfs:subPropertyOf*       <http://coralcea.ca/jasper/hrData>  .\n       }       UNION       {      ?dta           a                dta:DTA        .\n      ?dta           dta:operation    ?operation     .\n      ?operation     dta:kind         dta:Get        .\n      ?operation     dta:data/rdfs:subPropertyOf*       ?superRuri     .\n      ?superRuri     rdfs:range       ?superType     .\n      <http://coralcea.ca/jasper/hrData> rdfs:domain      ?superType     .\n   }}";
 		String result = classUnderTest.queryModel(queryString, "json");
 		TestCase.assertNotNull(result);
 		result = classUnderTest.queryModel(queryString, "xml");
